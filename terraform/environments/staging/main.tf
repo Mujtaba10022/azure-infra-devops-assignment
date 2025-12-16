ca5c1051dd8a288061a91dd6ad654e8f58435bda
Ghulam-Mujtaba_wkl <Ghulam.Mujtaba@wolterskluwer.com>
2025-12-16 21:46:49 +0000

    Create AKS using Azure CLI instead of Terraform

--- terraform/environments/staging/main.tf
+++ terraform/environments/staging/main.tf
@@ -3,39 +3,10 @@ data "azurerm_resource_group" "main" {
   name = "RG-GM_Assessment"
 }
 
-# AKS Cluster
-resource "azurerm_kubernetes_cluster" "main" {
-  name                = "aks-gm-staging"
-  location            = data. azurerm_resource_group. main.location
-  resource_group_name = data.azurerm_resource_group.main.name
-  dns_prefix          = "aksgmstaging"
-  kubernetes_version  = "1.27"
-
-  default_node_pool {
-    name       = "system"
-    node_count = 1
-    vm_size    = "Standard_B2s"
-  }
-
-  identity {
-    type = "SystemAssigned"
-  }
-
-  tags = {
-    Environment = "staging"
-    Project     = "GM-Assessment"
-    ManagedBy   = "Terraform"
-  }
-}
-
 output "resource_group_name" {
-  value = data.azurerm_resource_group.main.name
+  value = data.azurerm_resource_group.main. name
 }
 
-output "aks_cluster_name" {
-  value = azurerm_kubernetes_cluster.main. name
-}
-
-output "aks_cluster_id" {
-  value = azurerm_kubernetes_cluster.main.id
+output "status" {
+  value = "Infrastructure deployed - AKS created via Azure CLI"
 }
