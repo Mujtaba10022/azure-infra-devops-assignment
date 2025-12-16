c9e8d85ac18ed6c5f6430e85eef1bb74e8767c92
Ghulam-Mujtaba_wkl <Ghulam.Mujtaba@wolterskluwer.com>
2025-12-16 21:21:57 +0000

    Add AKS cluster

--- terraform/environments/staging/main.tf
+++ terraform/environments/staging/main.tf
@@ -1,25 +1,66 @@
 # Data source for existing resource group
 data "azurerm_resource_group" "main" {
-  name = var.  resource_group_name
+  name = var.resource_group_name
 }
 
-# Data source for current client  
+# Data source for current client
 data "azurerm_client_config" "current" {}
 
-# Note: Resources already exist in Azure - managed outside Terraform state
-# The following resources were created: 
-# - acrgmstaging (Container Registry)
-# - kv-gm-staging (Key Vault)  
-# - log-gm-staging (Log Analytics)
-# - oai-gm-staging (OpenAI)
-# - stgmstaging (Storage Account)
-# - vnet-gm-staging (Virtual Network)
-# - nsg-aks-eastus (Network Security Group)
-
-output "resource_group_name" {
-  value = data.azurerm_resource_group.main. name
+# Use existing Virtual Network
+data "azurerm_virtual_network" "main" {
+  name                = "vnet-gm-staging"
+  resource_group_name = data.azurerm_resource_group.main.name
 }
 
-output "resource_group_location" {
-  value = data. azurerm_resource_group. main.location
+# Use existing subnet or create new one for AKS
+resource "azurerm_subnet" "aks" {
+  name                 = "snet-aks"
+  resource_group_name  = data.azurerm_resource_group.main.name
+  virtual_network_name = data. azurerm_virtual_network. main.name
+  address_prefixes     = ["10.0.4.0/22"]
+}
+
+# AKS Cluster
+resource "azurerm_kubernetes_cluster" "main" {
+  name                = "aks-gm-staging"
+  location            = data.azurerm_resource_group.main.location
+  resource_group_name = data. azurerm_resource_group. main.name
+  dns_prefix          = "aks-gm-staging"
+  kubernetes_version  = "1.28"
+
+  default_node_pool {
+    name           = "system"
+    node_count     = 1
+    vm_size        = "Standard_B2s"
+    vnet_subnet_id = azurerm_subnet.aks.id
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  network_profile {
+    network_plugin    = "azure"
+    load_balancer_sku = "standard"
+    service_cidr      = "172.16.0.0/16"
+    dns_service_ip    = "172.16.0.10"
+  }
+
+  tags = {
+    Environment = "staging"
+    Project     = "GM-Assessment"
+    ManagedBy   = "Terraform"
+  }
+}
+
+output "resource_group_name" {
+  value = data.azurerm_resource_group.main.name
+}
+
+output "aks_cluster_name" {
+  value = azurerm_kubernetes_cluster.main.name
+}
+
+output "aks_cluster_id" {
+  value = azurerm_kubernetes_cluster.main.id
 }
