8260113991a869cfbfdeffab1da5c038d78881af
Ghulam-Mujtaba_wkl <Ghulam.Mujtaba@wolterskluwer.com>
2025-12-16 21:04:49 +0000

    Fix SQL region and simplify AKS module

--- terraform/modules/aks/main.tf
+++ terraform/modules/aks/main.tf
@@ -1,9 +1,9 @@
 resource "azurerm_kubernetes_cluster" "main" {
   name                = var.cluster_name
-  location            = var. location
+  location            = var.location
   resource_group_name = var.resource_group_name
   dns_prefix          = var.dns_prefix
-  kubernetes_version  = var.kubernetes_version
+  kubernetes_version  = var. kubernetes_version
 
   default_node_pool {
     name                = "system"
@@ -23,15 +23,9 @@ resource "azurerm_kubernetes_cluster" "main" {
 
   network_profile {
     network_plugin    = "azure"
-    network_policy    = "calico"
     load_balancer_sku = "standard"
     service_cidr      = "172.16.0.0/16"
     dns_service_ip    = "172.16.0.10"
-  }
-
-  azure_active_directory_role_based_access_control {
-    azure_rbac_enabled = true
-    managed            = true
   }
 
   key_vault_secrets_provider {
@@ -41,23 +35,8 @@ resource "azurerm_kubernetes_cluster" "main" {
   tags = var.tags
 }
 
-resource "azurerm_kubernetes_cluster_node_pool" "user" {
-  name                  = "user"
-  kubernetes_cluster_id = azurerm_kubernetes_cluster. main.id
-  vm_size               = var.user_node_vm_size
-  node_count            = var.user_node_count
-  vnet_subnet_id        = var.subnet_id
-  enable_auto_scaling   = true
-  min_count             = var.user_node_min_count
-  max_count             = var.user_node_max_count
-  os_disk_size_gb       = 128
-  mode                  = "User"
-
-  tags = var.tags
-}
-
 resource "azurerm_role_assignment" "acr_pull" {
-  count                            = var.attach_acr ? 1 :  0
+  count                            = var.attach_acr ?  1 : 0
   principal_id                     = azurerm_kubernetes_cluster.main.kubelet_identity[0].object_id
   role_definition_name             = "AcrPull"
   scope                            = var.acr_id
