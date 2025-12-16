05d713c02f22747990978fd7d40aaef8c43465fa
Ghulam-Mujtaba_wkl <Ghulam.Mujtaba@wolterskluwer.com>
2025-12-16 21:35:36 +0000

    Fix AKS - update provider and simplify config

--- terraform/environments/staging/main.tf
+++ terraform/environments/staging/main.tf
@@ -1,10 +1,7 @@
 # Data source for existing resource group
 data "azurerm_resource_group" "main" {
-  name = var. resource_group_name
+  name = "RG-GM_Assessment"
 }
-
-# Data source for current client
-data "azurerm_client_config" "current" {}
 
 # Use existing Virtual Network
 data "azurerm_virtual_network" "main" {
@@ -12,37 +9,29 @@ data "azurerm_virtual_network" "main" {
   resource_group_name = data.azurerm_resource_group.main.name
 }
 
-# Use existing AKS subnet (created in previous run)
+# Use existing AKS subnet
 data "azurerm_subnet" "aks" {
   name                 = "snet-aks"
   virtual_network_name = data.azurerm_virtual_network.main.name
-  resource_group_name  = data. azurerm_resource_group. main.name
+  resource_group_name  = data.azurerm_resource_group.main.name
 }
 
 # AKS Cluster
 resource "azurerm_kubernetes_cluster" "main" {
   name                = "aks-gm-staging"
-  location            = data. azurerm_resource_group. main.location
+  location            = data.azurerm_resource_group.main.location
   resource_group_name = data.azurerm_resource_group.main.name
-  dns_prefix          = "aks-gm-staging"
-  kubernetes_version  = "1.28"
+  dns_prefix          = "aksgmstaging"
+  kubernetes_version  = "1.27"
 
   default_node_pool {
-    name           = "system"
-    node_count     = 1
-    vm_size        = "Standard_B2s"
-    vnet_subnet_id = data.azurerm_subnet.aks.id
+    name       = "system"
+    node_count = 1
+    vm_size    = "Standard_B2s"
   }
 
   identity {
     type = "SystemAssigned"
-  }
-
-  network_profile {
-    network_plugin    = "azure"
-    load_balancer_sku = "standard"
-    service_cidr      = "172.16.0.0/16"
-    dns_service_ip    = "172.16.0.10"
   }
 
   tags = {
@@ -57,11 +46,9 @@ output "resource_group_name" {
 }
 
 output "aks_cluster_name" {
-  value = azurerm_kubernetes_cluster.main.name
+  value = azurerm_kubernetes_cluster. main.name
 }
 
 output "aks_cluster_id" {
   value = azurerm_kubernetes_cluster.main.id
 }
-
-# deploy aks
--- terraform/environments/staging/providers.tf
+++ terraform/environments/staging/providers.tf
@@ -1,23 +1,22 @@
 terraform {
-  required_version = ">= 1.5.0"
+  required_version = ">= 1.0.0"
 
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.85.0"
+      version = "~> 3.75.0"
     }
-    azuread = {
-      source  = "hashicorp/azuread"
-      version = "~> 2.46.0"
-    }
+  }
+
+  backend "azurerm" {
+    resource_group_name  = "RG-GM_Assessment"
+    storage_account_name = "stgmstaging"
+    container_name       = "tfstate"
+    key                  = "staging.terraform.tfstate"
   }
 }
 
 provider "azurerm" {
-  features {
-    key_vault {
-      purge_soft_delete_on_destroy = true
-    }
-  }
+  features {}
   skip_provider_registration = true
 }
