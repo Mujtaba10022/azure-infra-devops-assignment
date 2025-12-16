d3eb2c130a565188f48cf6156641225f59bdfb52
Ghulam-Mujtaba_wkl <Ghulam.Mujtaba@wolterskluwer.com>
2025-12-16 21:28:19 +0000

    Fix syntax - remove spaces in terraform references

--- terraform/environments/staging/main.tf
+++ terraform/environments/staging/main.tf
@@ -1,6 +1,6 @@
 # Data source for existing resource group
 data "azurerm_resource_group" "main" {
-  name = var.resource_group_name
+  name = var. resource_group_name
 }
 
 # Data source for current client
@@ -12,19 +12,18 @@ data "azurerm_virtual_network" "main" {
   resource_group_name = data.azurerm_resource_group.main.name
 }
 
-# Use existing subnet or create new one for AKS
-resource "azurerm_subnet" "aks" {
+# Use existing AKS subnet (created in previous run)
+data "azurerm_subnet" "aks" {
   name                 = "snet-aks"
-  resource_group_name  = data.azurerm_resource_group.main.name
-  virtual_network_name = data. azurerm_virtual_network. main.name
-  address_prefixes     = ["10.0.4.0/22"]
+  virtual_network_name = data.azurerm_virtual_network.main.name
+  resource_group_name  = data. azurerm_resource_group. main.name
 }
 
 # AKS Cluster
 resource "azurerm_kubernetes_cluster" "main" {
   name                = "aks-gm-staging"
-  location            = data.azurerm_resource_group.main.location
-  resource_group_name = data. azurerm_resource_group. main.name
+  location            = data. azurerm_resource_group. main.location
+  resource_group_name = data.azurerm_resource_group.main.name
   dns_prefix          = "aks-gm-staging"
   kubernetes_version  = "1.28"
 
@@ -32,7 +31,7 @@ resource "azurerm_kubernetes_cluster" "main" {
     name           = "system"
     node_count     = 1
     vm_size        = "Standard_B2s"
-    vnet_subnet_id = azurerm_subnet.aks.id
+    vnet_subnet_id = data.azurerm_subnet.aks.id
   }
 
   identity {
@@ -64,5 +63,3 @@ output "aks_cluster_name" {
 output "aks_cluster_id" {
   value = azurerm_kubernetes_cluster.main.id
 }
-
-# trigger aks
